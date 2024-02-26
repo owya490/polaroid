802873b76aa017dbe5ac899c7eeaaf657885e323
Owen <owen.yang@sowaustralia.com>
2024-02-27 00:03:27 +1100

    resolve viewport zooming issue

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -32,6 +32,7 @@ export default function Home() {
   const [allPolaroids, setAllPolaroids] = useState<PolaroidType[]>([]);
   const [isNotLoggedIn, setIsNotLoggedIn] = useState(true);
   const [passcode, setPasscode] = useState("");
+  
 
   useEffect(() => {
     getAllPolaroids().then((data) => {
